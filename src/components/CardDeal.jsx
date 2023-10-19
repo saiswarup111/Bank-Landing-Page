5ddf255943bd9cb6c7e791e86bafc3e97f4009cc
saiswarup111 <saiswarupmohanty920@gmail.com>
2023-10-19 19:32:30 +0530

    readme updated

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -5,6 +5,7 @@ import Button from "./Button";
 const CardDeal = () => (
   <section className={layout.section}>
     <div className={layout.sectionInfo}>
+      
       <h2 className={styles.heading2}>
         Find a better card deal <br className="sm:block hidden" /> in few easy
         steps.
